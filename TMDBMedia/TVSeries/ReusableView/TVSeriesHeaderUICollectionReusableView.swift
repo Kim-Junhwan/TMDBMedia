d593c98a5920c2d0b0af747c75cdd48923182993
Kim junhwan <tazanboy1@naver.com>
2023-08-19 02:13:34 +0900

    [refactor] TV시리즈 헤더뷰 ReusableIdentifier 채택 및 재사용 시 이미지 제거

--- TMDBMedia/TVSeries/ReusableView/TVSeriesHeaderUICollectionReusableView.swift
+++ TMDBMedia/TVSeries/ReusableView/TVSeriesHeaderUICollectionReusableView.swift
@@ -7,19 +7,25 @@
 
 import UIKit
 
-class TVSeriesHeaderUICollectionReusableView: UICollectionReusableView {
+class TVSeriesHeaderUICollectionReusableView: UICollectionReusableView, ReusableIdentifier {
     
     @IBOutlet weak var backdropImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        nameLabel.adjustsFontSizeToFitWidth = true
     }
     
     func configureHeaderView(tvseries: TVSeries) {
         nameLabel.text = tvseries.name
         backdropImage.getImageFromUrl(posterPath: tvseries.backdropPath)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        backdropImage.image = nil
     }
     
 }
