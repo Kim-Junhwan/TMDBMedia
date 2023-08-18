dd310c754c166b9f9c2a7139eed935dbf8a086cc
Kim junhwan <tazanboy1@naver.com>
2023-08-19 02:17:36 +0900

    [feat] TV시리즈 시즌 셀 구현

---
+++ TMDBMedia/TVSeries/Cell/TVSeriesCollectionViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  TVSeriesCollectionViewCell.swift
+//  TMDBMedia
+//
+//  Created by JunHwan Kim on 2023/08/17.
+//
+
+import UIKit
+import Alamofire
+
+class TVSeriesCollectionViewCell: UICollectionViewCell, ReusableIdentifier {
+
+    @IBOutlet weak var seasonNameLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var episodeList: EpisodeList = EpisodeList(episodes: [])
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: TVSeriesDetailSeasonCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TVSeriesDetailSeasonCollectionViewCell.identifier)
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumInteritemSpacing = 10.0
+        flowLayout.itemSize = CGSize(width: collectionView.frame.height, height: collectionView.frame.height)
+        flowLayout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = flowLayout
+    }
+    
+    func configureCell(season: Season, seasonId: Int) {
+        seasonNameLabel.text = season.name
+        AF.request("https://api.themoviedb.org/3/tv/\(seasonId)/season/\(season.seasonNumber)?api_key=8b221a8ca88f8a965a6496655ac80a90").validate().responseDecodable(of: EpisodeList.self) { response in
+            guard let episodes = response.value else { return }
+            self.episodeList = episodes
+            self.collectionView.reloadData()
+        }
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        print("reuse")
+    }
+}
+
+extension TVSeriesCollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return episodeList.episodes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TVSeriesDetailSeasonCollectionViewCell.identifier, for: indexPath) as? TVSeriesDetailSeasonCollectionViewCell else { return TVSeriesCollectionViewCell() }
+        cell.configureCell(episode: episodeList.episodes[indexPath.row])
+        
+        return cell
+    }
+    
+    
+}
