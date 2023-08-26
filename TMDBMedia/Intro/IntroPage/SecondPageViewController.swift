5d89de96bf34c9a023594fbd28c8411161e35448
Kim junhwan <tazanboy1@naver.com>
2023-08-26 22:27:48 +0900

    [feat] 인트로 페이지 화면 구현

---
+++ TMDBMedia/Intro/IntroPage/SecondPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondPageViewController.swift
+//  TMDBMedia
+//
+//  Created by JunHwan Kim on 2023/08/26.
+//
+
+import UIKit
+
+class SecondPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TMDBMedia/UserDefaults/UserDefaultRepository.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultRepository.swift
+//  TMDBMedia
+//
+//  Created by JunHwan Kim on 2023/08/26.
+//
+
+import Foundation
