577d5fece1e35648ae9c5803c7601c217c9455d6
Kim junhwan <tazanboy1@naver.com>
2023-09-04 02:22:24 +0900

    [feat] TMDB API 통신하는 repository 구현

---
+++ TMDBMedia/Data/Repositories/DefaultTMDBRepository.swift
@@ -0,0 +1,31 @@
+//
+//  DefaultTMDBRepository.swift
+//  TMDBMedia
+//
+//  Created by JunHwan Kim on 2023/09/02.
+//
+
+import Foundation
+
+final class DefaultTMDBRepository {
+    
+    private let dataTransferService: DataTransferService
+    
+    init(dataTransferService: DataTransferService) {
+        self.dataTransferService = dataTransferService
+    }
+}
+
+extension DefaultTMDBRepository: TMDBRepository {
+    func fetchTrendList(page: Int, completion: @escaping (Result<TrendPage, Error>) -> Void) {
+        let endpoint = APIEndpoints.fetchTrendList(trendListRequestDTO: .init(page: page))
+        dataTransferService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data.toDomain()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
