d86e88ce4f4fe76192bc3893bb945366a5a57206
Kim junhwan <tazanboy1@naver.com>
2023-09-03 15:53:01 +0900

    [feat] 네트워크 서비스 계층 구현

---
+++ TMDBMedia/Network/NetworkService.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkService.swift
+//  TMDBMedia
+//
+//  Created by JunHwan Kim on 2023/09/02.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case responseError(statusCode: Int, data: Data?)
+    case url
+}
+
+protocol NetworkService {
+    func request(endPoint: EndPoint,completion: @escaping (Result<Data?, NetworkError>) -> Void)
+}
+
+final class DefaultNetworkService {
+    
+    private let config: NetworkConfiguration
+    
+    init(config: NetworkConfiguration) {
+        self.config = config
+    }
+}
+
+extension DefaultNetworkService: NetworkService {
+    
+    func request(endPoint: EndPoint ,completion: @escaping (Result<Data?, NetworkError>) -> Void) {
+        do {
+            let request = try endPoint.makeURLRequest(networkConfig: config)
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    if let response = response as? HTTPURLResponse {
+                        completion(.failure(.responseError(statusCode: response.statusCode, data: data)))
+                    }
+                } else {
+                    completion(.success(data))
+                }
+            }
+        } catch {
+            completion(.failure(.url))
+        }
+    }
+    
+    
+}
