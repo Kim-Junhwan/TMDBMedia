6dce0877b4d06b7df51b4fec7b9246b891d5eebd
Kim junhwan <tazanboy1@naver.com>
2023-09-03 22:48:47 +0900

    [refactor] NetworkService endpoint 타입을 Requestable로 변경

--- TMDBMedia/Network/NetworkService.swift
+++ TMDBMedia/Network/NetworkService.swift
@@ -9,11 +9,12 @@ import Foundation
 
 enum NetworkError: Error {
     case responseError(statusCode: Int, data: Data?)
+    case networkError(Error)
     case url
 }
 
 protocol NetworkService {
-    func request(endPoint: EndPoint,completion: @escaping (Result<Data?, NetworkError>) -> Void)
+    func request(endPoint: Requestable ,completion: @escaping (Result<Data?, NetworkError>) -> Void)
 }
 
 final class DefaultNetworkService {
@@ -27,13 +28,15 @@ final class DefaultNetworkService {
 
 extension DefaultNetworkService: NetworkService {
     
-    func request(endPoint: EndPoint ,completion: @escaping (Result<Data?, NetworkError>) -> Void) {
+    func request(endPoint: Requestable ,completion: @escaping (Result<Data?, NetworkError>) -> Void) {
         do {
             let request = try endPoint.makeURLRequest(networkConfig: config)
             URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error {
+                if let error = error {
                     if let response = response as? HTTPURLResponse {
                         completion(.failure(.responseError(statusCode: response.statusCode, data: data)))
+                    } else {
+                        completion(.failure(.networkError(error)))
                     }
                 } else {
                     completion(.success(data))
