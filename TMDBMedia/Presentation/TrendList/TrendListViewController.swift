271057129262f292616fb098c906651c7a1a0dc5
Kim junhwan <tazanboy1@naver.com>
2023-09-04 19:15:19 +0900

    [refactor] TrendList 리스트 가져오는 코드 분리

--- TMDBMedia/Presentation/TrendList/TrendListViewController.swift
+++ TMDBMedia/Presentation/TrendList/TrendListViewController.swift
@@ -11,9 +11,27 @@ import Alamofire
 class TrendListViewController: BaseViewController {
     
     let trendListView = TrendListView()
-
+    var trendList: [Media] = []
+    var currentPage = 0
+    var totalPage = 1
+    var hasNextPage: Bool { currentPage < totalPage }
+    var nextPage: Int {
+        get {
+            hasNextPage ? currentPage+1 : currentPage
+        }
+    }
     
-    var movieList: MovieList = MovieList(results: [])
+    let repository: TMDBRepository
+    
+    init(repository: TMDBRepository) {
+        self.repository = repository
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = trendListView
@@ -22,6 +40,7 @@ class TrendListViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.tintColor = .black
+        title = "Trend List"
     }
     
     override func viewDidLayoutSubviews() {
@@ -30,7 +49,7 @@ class TrendListViewController: BaseViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        fetchMovieList()
+        fetchTrendList()
     }
     
     override func configureView() {
@@ -40,11 +59,19 @@ class TrendListViewController: BaseViewController {
         trendListView.collectionView.dataSource = self
     }
     
-    func fetchMovieList() {
-        AF.request("https://api.themoviedb.org/3/trending/movie/day?api_key=\(APIKey.tmdsAPIKey)&language=ko-KR").responseDecodable(of: MovieList.self) { [weak self] response in
-            guard let fetchList = response.value else { return }
-            self?.movieList = fetchList
-            self?.trendListView.collectionView.reloadData()
+    func fetchTrendList() {
+        repository.fetchTrendList(page: nextPage) { [weak self] result in
+            switch result {
+            case .success(let trendPage):
+                self?.currentPage = trendPage.page
+                self?.totalPage = trendPage.totalPages
+                self?.trendList.append(contentsOf: trendPage.mediaList)
+                DispatchQueue.main.async {
+                    self?.trendListView.collectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
         }
     }
     
@@ -58,20 +85,21 @@ class TrendListViewController: BaseViewController {
 extension TrendListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movieList.count
+        return trendList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrendListCollectionViewCell.identifier, for: indexPath) as? TrendListCollectionViewCell else { return UICollectionViewCell() }
-        cell.configureCell(movie: movieList.getMovieForIndex(index: indexPath.row))
+        cell.configureCell(media: trendList[indexPath.row])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let detailViewController = DetailViewController()
-        detailViewController.movie = movieList.getMovieForIndex(index: indexPath.row)
+        //detailViewController.movie = movieList.getMovieForIndex(index: indexPath.row)
         navigationController?.pushViewController(detailViewController, animated: true)
     }
+    
+    
     
 }
