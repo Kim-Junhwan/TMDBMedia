f0917c1ce62d752d977fdc51c24da7839158aeeb
Kim junhwan <tazanboy1@naver.com>
2023-09-04 20:56:33 +0900

    [feat] 상세 정보 가져오는 Endpoint 추가

--- TMDBMedia/Data/Endpoints/APIEndpoints.swift
+++ TMDBMedia/Data/Endpoints/APIEndpoints.swift
@@ -11,4 +11,8 @@ struct APIEndpoints {
     static func fetchTrendList(trendListRequestDTO: TrendListRequestDTO) -> EndPoint<TrendListResponseDTO> {
         return EndPoint(path: "/3/trending/all/day", method: .GET, query: trendListRequestDTO)
     }
+    
+    static func fetchDetailMedia(detailReqeustDTO: DetailMediaRequestDTO) -> EndPoint<DetailMediaResponseDTO> {
+        return EndPoint(path: "3/movie/\(detailReqeustDTO.id)/credits", method: .GET, query: nil)
+    }
 }
