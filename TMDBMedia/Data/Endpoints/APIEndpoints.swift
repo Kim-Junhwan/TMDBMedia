08531ab706493d149724fbda9f0532c2d14cba78
Kim junhwan <tazanboy1@naver.com>
2023-09-04 19:22:34 +0900

    [fix] 쿼리문 적용 안되던 버그 픽스

--- TMDBMedia/Data/Endpoints/APIEndpoints.swift
+++ TMDBMedia/Data/Endpoints/APIEndpoints.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct APIEndpoints {
     static func fetchTrendList(trendListRequestDTO: TrendListRequestDTO) -> EndPoint<TrendListResponseDTO> {
-        return EndPoint(path: "/3/trending/all/day", method: .GET)
+        return EndPoint(path: "/3/trending/all/day", method: .GET, query: trendListRequestDTO)
     }
 }
