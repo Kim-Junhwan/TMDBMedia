74e071f7a32e44e59ab608a3156dde24ce4b3606
Kim junhwan <tazanboy1@naver.com>
2023-09-05 17:49:43 +0900

    [refactor] tv시리즈 가져오는 네트워크 코드 뷰컨트롤러에서  분리

--- TMDBMedia/Presentation/TVSeries/TVSeriesViewController.swift
+++ TMDBMedia/Presentation/TVSeries/TVSeriesViewController.swift
@@ -8,39 +8,66 @@
 import UIKit
 import Alamofire
 
-class TVSeriesViewController: UIViewController {
+class TVSeriesViewController: BaseViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+        collectionView.register(UINib(nibName: TrendListCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TrendListCollectionViewCell.identifier)
+        collectionView.backgroundColor = .black
+        return collectionView
+    }()
     
-    var tvseriesList: TVSeriesList = TVSeriesList()
-    var sectionTVSeriesList: [Int:TVSeriesSeason] = [:]
+    var isFetching = false
+    var tvseriesList: [TVSeries] = []
+    var repository: TMDBRepository
+    var currentPage = 0
+    var totalPage = 1
+    var hasNextPage: Bool { currentPage < totalPage }
+    var nextPage: Int {
+        get {
+            hasNextPage ? currentPage+1 : currentPage
+        }
+    }
+    
+    init(repository: TMDBRepository) {
+        self.repository = repository
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setCollectionView()
         title = "TVSeries"
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor:UIColor.white]
-        
-        let group = DispatchGroup()
-        group.enter()
-        AF.request("https://api.themoviedb.org/3/tv/top_rated?api_key=\(APIKey.tmdsAPIKey)", method: .get).validate().responseDecodable(of: TVSeriesList.self) { [weak self] result in
-            guard let list = result.value else { return }
-            self?.tvseriesList = list
-            for tvSeries in list.results {
-                DispatchQueue.global().async {
-                    group.enter()
-                    AF.request("https://api.themoviedb.org/3/tv/\(tvSeries.id)?api_key=\(APIKey.tmdsAPIKey)&language=ko-KR").validate().responseDecodable(of: TVSeriesSeason.self) { response in
-                        guard let fetchTvSeriesSeason = response.value else { return }
-                        self?.sectionTVSeriesList[tvSeries.id] = fetchTvSeriesSeason
-                        group.leave()
-                    }
-                }
+        fetchTVSeries()
+    }
+    
+    override func configureView() {
+        view.addSubview(collectionView)
+    }
+    
+    override func setConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func fetchTVSeries() {
+        repository.fetchTVSeriesList(page: nextPage, completion: { [weak self] result in
+            switch result {
+            case .success(let tvSeries):
+                self?.currentPage = tvSeries.page
+                self?.totalPage = tvSeries.totalPages
+                self?.tvseriesList.append(contentsOf: tvSeries.tvSeriesList)
+                self?.collectionView.reloadData()
+            case .failure(let failure):
+                print(failure)
             }
-            group.leave()
-        }
-        group.notify(queue: .main) {
-            self.collectionView.reloadData()
-        }
+        })
     }
     
     func setCollectionView() {
@@ -49,6 +76,10 @@ class TVSeriesViewController: UIViewController {
         collectionView.register(UINib(nibName: TVSeriesHeaderUICollectionReusableView.identifier, bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: TVSeriesHeaderUICollectionReusableView.identifier)
         
         collectionView.register(UINib(nibName: TVSeriesCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TVSeriesCollectionViewCell.identifier)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         let flowlayout = UICollectionViewFlowLayout()
         flowlayout.minimumLineSpacing = 8
         flowlayout.minimumInteritemSpacing = 8
@@ -58,31 +89,38 @@ class TVSeriesViewController: UIViewController {
         flowlayout.scrollDirection = .vertical
         collectionView.collectionViewLayout = flowlayout
     }
-    
 }
 
 extension TVSeriesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
+
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return tvseriesList.results.count
+        return tvseriesList.count
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sectionTVSeriesList[tvseriesList.results[section].id]?.seasons.count ?? 0
+        return tvseriesList[section].seasons.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TVSeriesCollectionViewCell.identifier, for: indexPath) as? TVSeriesCollectionViewCell else { return UICollectionViewCell() }
-        let tvSeriesId = tvseriesList.results[indexPath.section].id
-        guard let season = sectionTVSeriesList[tvSeriesId]?.seasons[indexPath.row] else { return UICollectionViewCell() }
-        cell.configureCell(season: season, seasonId: tvSeriesId)
+        let tvSeriesSeason = tvseriesList[indexPath.section].seasons[indexPath.row]
+        cell.configureCell(tvSeriesId: tvseriesList[indexPath.section].id, season: tvSeriesSeason, repository: repository)
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         guard let reusableView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: TVSeriesHeaderUICollectionReusableView.identifier, for: indexPath) as? TVSeriesHeaderUICollectionReusableView else { return .init() }
-        reusableView.configureHeaderView(tvseries: tvseriesList.results[indexPath.section])
+        reusableView.configureHeaderView(tvseries: tvseriesList[indexPath.section])
         return reusableView
     }
 
+}
+
+extension TVSeriesViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y >=  (scrollView.contentSize.height-(scrollView.frame.height-tabBarController!.tabBar.frame.height)) && !isFetching{
+            fetchTVSeries()
+        }
+    }
+    
 }
